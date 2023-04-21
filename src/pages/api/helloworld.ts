efd9d775ea1f6d26ddf82f74e4b9143aae5f5b27
Omari <marzipant.e@gmail.com>
2023-04-21 13:57:16 +0100

    Add API route

---
+++ src/pages/api/helloworld.ts
@@ -0,0 +1,5 @@
+//src/pages/api/helloworld
+import type { NextApiRequest, NextApiResponse } from 'next';
+export default function handler(req: NextApiRequest, res: NextApiResponse) {
+    res.status(200).send('Hello world!');
+}
